2a9fb13ed50ace4c7579f70de5e5bcf21b30e98f
Mark Gekelman <gekelmam@oregonstate.edu>
2021-03-11 09:21:50 -0800

    Fixing lint again

--- calculator.py
+++ calculator.py
@@ -13,3 +13,4 @@ def subtract(a, b):
 
 def multiply(a, b):
     return a * b
+
--- test_calculator.py
+++ test_calculator.py
@@ -16,5 +16,6 @@ class TestCalculator:
     def test_multiply(self):
         assert 2 == calculator.multiply(1, 2)
 
+
 def test_multiply(self):
     assert 100 == calculator.multiply(10, 10)
